9e13922ddd1d1035befd6257de8f04de5b94071d
Tor Magne <tormagne-brekken@outlook.com>
2021-11-25 20:16:08 +0100

    commit

--- js/ui/wishesFunctions.js
+++
@@ -1,9 +0,0 @@
-export const getExsitingWhistlist = () => {
-  const wishes = localStorage.getItem('wishlist');
-
-  if (!wishes) {
-    return [];
-  } else {
-    return JSON.parse(wishes);
-  }
-};
